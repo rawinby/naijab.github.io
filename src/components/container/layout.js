04e46aee176003f3c8c1ba035744239b8d6620b3
Nattapon Pondongnok <nainatjab999@gmail.com>
2019-03-04 03:54:33 +0700

    Check is post detail

--- src/components/container/layout.js
+++ src/components/container/layout.js
@@ -1,11 +1,16 @@
 import React, { Fragment } from "react"
 import { Container } from "reactstrap"
+import NavBar from "../menu/navbar"
 import Footer from "../footer/footer"
 
 const Layout = props => {
+  const containerStyle = props.isPostDetail
+    ? "default-container"
+    : "home-container"
   return (
     <Fragment>
-      <Container className="default-container">{props.children}</Container>
+      <NavBar />
+      <Container className={containerStyle}>{props.children}</Container>
       <Footer />
     </Fragment>
   )
