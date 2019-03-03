7a59d75d4ef06c7423a1ecec10dcc85611a9cfd4
Nattapon Pondongnok <nainatjab999@gmail.com>
2019-03-04 00:03:16 +0700

    Add prismjs

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,4 @@
 import "bootstrap/dist/css/bootstrap.css"
 import "./content/assets/css/style.scss"
+import "prismjs/themes/prism-tomorrow.css"
+import "prismjs/plugins/line-numbers/prism-line-numbers.css"
