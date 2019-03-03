959e7cc8a2dccff35b55764067f2cf91d68ec971
Nattapon Pondongnok <nainatjab999@gmail.com>
2019-03-04 04:14:12 +0700

    Add id and isHome props

--- src/components/post/post-list.js
+++ src/components/post/post-list.js
@@ -2,17 +2,29 @@ import React, { Fragment } from "react"
 import PropTypes from "prop-types"
 import { If, Then, Else } from "react-if"
 import PostItem from "./post-item"
+import { Row } from "reactstrap"
 
-const PostList = ({ posts }) => {
+const PostList = ({ posts, isHome }) => {
   return (
     <Fragment>
       <If condition={posts.length > 0}>
         <Then>
-          {posts.map(({ node }, key) => {
-            const { title, date } = node.frontmatter
-            const { slug } = node.fields
-            return <PostItem title={title} date={date} slug={slug} key={key} />
-          })}
+          <Row>
+            {posts.map(({ node }, key) => {
+              const { title, date } = node.frontmatter
+              const { slug } = node.fields
+              return (
+                <PostItem
+                  title={title}
+                  date={date}
+                  slug={slug}
+                  key={key}
+                  id={key}
+                  isHome={isHome}
+                />
+              )
+            })}
+          </Row>
         </Then>
         <Else>
           <h3>No Post..</h3>
