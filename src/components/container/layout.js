f0f78fdc155992522f91c3a4675b39fd73f645f3
Nattapon Pondongnok <nainatjab999@gmail.com>
2019-03-04 08:13:06 +0700

    Move seo to layout

--- src/components/container/layout.js
+++ src/components/container/layout.js
@@ -2,13 +2,17 @@ import React, { Fragment } from "react"
 import { Container } from "reactstrap"
 import NavBar from "../menu/navbar"
 import Footer from "../footer/footer"
+import SEO from "../seo"
 
 const Layout = props => {
-  const containerStyle = props.isPostDetail
-    ? "default-container"
-    : "home-container"
+  const containerStyle = props.isHome ? "home-container" : "default-container"
   return (
     <Fragment>
+      <SEO
+        title={props.title}
+        description={props.description}
+        keywords={props.keywords}
+      />
       <NavBar />
       <Container className={containerStyle}>{props.children}</Container>
       <Footer />
