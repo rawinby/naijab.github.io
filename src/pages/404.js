85e44f887d131491d89abc1b63f90e3368da855d
Nattapon Pondongnok <nainatjab999@gmail.com>
2019-03-04 08:11:18 +0700

    Use functional instead class

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,34 +1,17 @@
 import React from "react"
-import { graphql, Link } from "gatsby"
+import { Link } from "gatsby"
 import Layout from "../components/container/layout"
-import SEO from "../components/seo"
 
-class NotFoundPage extends React.Component {
-  render() {
-    const { data } = this.props
-    const siteTitle = data.site.siteMetadata.title
-
-    return (
-      <Layout location={this.props.location} title={siteTitle} isPostDetail>
-        <SEO title="404 Not Found" />
-        <h1>404 Not Found</h1>
-        <p>
-          ขออภัยไม่พบหน้าที่ต้องการ :( <br />{" "}
-          <Link to="/all">ลองค้นหาบทความ</Link>
-        </p>
-      </Layout>
-    )
-  }
+const NotFoundPage = props => {
+  return (
+    <Layout title="404 Not Found" location={props.location} isPostDetail>
+      <h1>404 Not Found</h1>
+      <p>
+        ขออภัยไม่พบหน้าที่ต้องการ :( <br />{" "}
+        <Link to="/all">ลองค้นหาบทความ</Link>
+      </p>
+    </Layout>
+  )
 }
 
 export default NotFoundPage
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`
