fd846d2a9eb85054bad6cd07bf3aa521d2c73307
Nattapon Pondongnok <nainatjab999@gmail.com>
2019-03-05 12:18:34 +0700

    Use bootstrap version minify

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,4 @@
-import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap/dist/css/bootstrap.min.css"
 import "./content/assets/css/style.scss"
 import "prismjs/themes/prism-tomorrow.css"
 import "prismjs/plugins/line-numbers/prism-line-numbers.css"
