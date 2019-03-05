95773512524dac9d625a91260d30f8c4e50ca809
Nattapon Pondongnok <nainatjab999@gmail.com>
2019-03-05 19:01:17 +0700

    Add pagination component

---
+++ src/components/menu/pagination.js
@@ -0,0 +1,27 @@
+import React from "react"
+import { Link } from "gatsby"
+import { Button } from "reactstrap"
+
+const Pagination = ({ isFirst, isLast, prevPage, nextPage }) => {
+  return (
+    <div className="navigation-container">
+      {!isFirst && (
+        <Link to={prevPage}>
+          <Button size="lg" className="float-left">
+            {" << ใหม่กว่านี้"}
+          </Button>
+        </Link>
+      )}
+
+      {!isLast && (
+        <Link to={nextPage}>
+          <Button size="lg" className="float-right">
+            {"เก่ากว่านี้ >> "}
+          </Button>
+        </Link>
+      )}
+    </div>
+  )
+}
+
+export default Pagination
